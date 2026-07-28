02a87448a8adad49c2508c5de939a8603533655e
Beuuuh <belldiscord8768@hotmail.com>
2026-07-28 20:15:36 -0300

    fix: bug fixes

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-#include "stdargs.h"
+#include <stdarg.h>
 #include "esp_log.h"
 #include <freertos/mpu_wrappers.h>
 #include "esp_vfs_fat.h"
 #include "sdmmc_cmd.h"
+#include "LogManager.h"
 
 #define MOUNT_POINT "/sdcard"
 #define MISO_PIN 2
@@ -51,10 +52,10 @@ extern "C" {
         return ESP_OK;
     }
 
-    vprintf_like_t log_output_changer(const char* esp_log, va_list args) {
+    int log_output_changer(const char* esp_log, va_list args) {
         char buffer[256];
         vsnprintf(buffer, sizeof(buffer), esp_log, args); 
-        LogManager::getInstance().process_log(esp_log, args);
+        LogManager::getInstance();
 
         return vprintf(esp_log, args);
     }
@@ -65,7 +66,7 @@ extern "C" {
         ESP_LOGI(TAG, "before using esp_log_set_vprintf");
         esp_log_set_vprintf(log_output_changer);
 
-        ESP_LOGI(TAG, "This is now being shown in stdout instead of UART")
+        ESP_LOGI(TAG, "This is now being shown in stdout instead of UART");
 
     }
 }
