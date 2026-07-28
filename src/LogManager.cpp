0d4cd99c731f0d37dfd57277cace12b74b4484fd
Beuuuh <belldiscord8768@hotmail.com>
2026-07-28 20:16:11 -0300

    refactor: changes how LogManager.cpp is written

--- src/LogManager.cpp
+++ src/LogManager.cpp
@@ -1,10 +1,19 @@
 #include "stdarg.h"
+#include <stdio.h>
+#include "LogManager.h"
 
 LogManager &LogManager::getInstance() {
-    static LogManager instance; 
+    static LogManager instance;
     return instance;
 }
 
-void LogManager::process_log(const char* fmt, va_list args) {
-
+void LogManager::process_log(const char* format, va_list args) {
+    char buffer[256];
+    vsnprintf(buffer, sizeof(buffer), format, args);
+    printf("%s", buffer);
 }
+
+void LogManager::init() {
+    printf("LogManager initialized\n");
+}
+
--- src/LogManager.h
+++ src/LogManager.h
@@ -4,11 +4,12 @@ class LogManager {
     private:
         //Singleton stuff
         LogManager() {
-            
+            init();
         }
 
         LogManager(const LogManager&) = delete;
         LogManager& operator=(const LogManager&) = delete;
         
-        void process_log();
+        void process_log(const char* format, va_list args);
+        void init();
 };
