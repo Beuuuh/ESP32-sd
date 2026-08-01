e12df6404275ad68ff4a91a6ecbe32e67aa36582
Beuuuh <belldiscord8768@hotmail.com>
2026-08-01 10:43:53 -0300

    fix: changes visibility of init()

--- src/LogManager.h
+++ src/LogManager.h
@@ -7,16 +7,16 @@ class LogManager {
     public:
         static LogManager &getInstance();
         void process_log(const char* format, va_list args);
+        void init();
     private:
         //Singleton stuff
         LogManager() {
-            init();
+            
         }
 
         LogManager(const LogManager&) = delete;
         LogManager& operator=(const LogManager&) = delete;
         
-        void init();
         esp_err_t writeFile(const char *path, char *data);
         esp_err_t readFile(const char *path);
         void configSDCard();
